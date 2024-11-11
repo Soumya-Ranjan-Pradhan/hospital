511516fb504c6a5e3b335c9caee80ff007b16669
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2024-11-11 22:30:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import CardSection from "./Components/CardSection";
+import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
+import Services from "./Components/Services";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <CardSection />
+      <Services />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Services.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div className="bg-[#004CAA] min-h-screen py-10">
+      <h1 className="text-white text-3xl font-bold text-center mb-10">
+        Our Services
+      </h1>
+      <div className=" px-5 grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {/* 1stBox */}
+        <div className="bg-[#004CAA] hover:bg-[#4498ff] text-white p-14 rounded-lg shadow-lg border border-gray-500 flex flex-col items-center">
+          <div className="text-5xl mb-4">🦷</div>
+          <h2 className="text-xl font-bold mb-2">Complete Dentistry</h2>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            dignissimos non maiores?
+          </p>
+          <button className="mt-auto py-2 px-4 border border-white rounded hover:bg-white hover:text-blue-900 transition">
+            Read More
+          </button>
+        </div>
+
+        {/* 2stBox */}
+        <div className="bg-[#004CAA] hover:bg-[#4498ff] text-white p-14 rounded-lg shadow-lg border border-gray-500 flex flex-col items-center">
+          <div className="text-5xl mb-4">🦷</div>
+          <h2 className="text-xl font-bold mb-2">Complete Dentistry</h2>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            dignissimos non maiores?
+          </p>
+          <button className="mt-auto py-2 px-4 border border-white rounded hover:bg-white hover:text-blue-900 transition">
+            Read More
+          </button>
+        </div>
+
+        {/* 3stBox */}
+        <div className="bg-[#004CAA] hover:bg-[#4498ff] text-white p-14 rounded-lg shadow-lg border border-gray-500 flex flex-col items-center">
+          <div className="text-5xl mb-4">🦷</div>
+          <h2 className="text-xl font-bold mb-2">Complete Dentistry</h2>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            dignissimos non maiores?
+          </p>
+          <button className="mt-auto py-2 px-4 border border-white rounded hover:bg-white hover:text-blue-900 transition">
+            Read More
+          </button>
+        </div>
+
+        {/* 4stBox */}
+        <div className="bg-[#004CAA] hover:bg-[#4498ff] text-white p-14 rounded-lg shadow-lg border border-gray-500 flex flex-col items-center">
+          <div className="text-5xl mb-4">🦷</div>
+          <h2 className="text-xl font-bold mb-2">Complete Dentistry</h2>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            dignissimos non maiores?
+          </p>
+          <button className="mt-auto py-2 px-4 border border-white rounded hover:bg-white hover:text-blue-900 transition">
+            Read More
+          </button>
+        </div>
+
+        {/* 5stBox */}
+        <div className="bg-[#004CAA] hover:bg-[#4498ff] text-white p-14 rounded-lg shadow-lg border border-gray-500 flex flex-col items-center">
+          <div className="text-5xl mb-4">🦷</div>
+          <h2 className="text-xl font-bold mb-2">Complete Dentistry</h2>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            dignissimos non maiores?
+          </p>
+          <button className="mt-auto py-2 px-4 border border-white rounded hover:bg-white hover:text-blue-900 transition">
+            Read More
+          </button>
+        </div>
+
+        {/* 6stBox */}
+        <div className="bg-[#004CAA] hover:bg-[#4498ff] text-white p-14 rounded-lg shadow-lg border border-gray-500 flex flex-col items-center">
+          <div className="text-5xl mb-4">🦷</div>
+          <h2 className="text-xl font-bold mb-2">Complete Dentistry</h2>
+          <p className="text-center mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id
+            dignissimos non maiores?
+          </p>
+          <button className="mt-auto py-2 px-4 border border-white rounded hover:bg-white hover:text-blue-900 transition">
+            Read More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
